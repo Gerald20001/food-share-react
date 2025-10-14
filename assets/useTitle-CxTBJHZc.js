626df454106d38ffe0e70f1ecbae633c6a664b34
Griffith <igorslobodan05@gmail.com>
2025-10-14 09:44:17 +0300

    Updates

---
+++ assets/useTitle-CxTBJHZc.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-yKh5yMux.js";const t="FoodShare | Connecting Communities";function r(e){o.useEffect(()=>(document.title=e?`${e} | FoodShare`:t,()=>{document.title=t}),[e])}export{r as u};
