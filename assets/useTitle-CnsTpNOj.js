85f28524d8cd94365d25c13302f7f851af7f4080
Griffith <igorslobodan05@gmail.com>
2025-10-14 10:07:50 +0300

    Updates

---
+++ assets/useTitle-CnsTpNOj.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-BLSWsbFh.js";const t="FoodShare | Connecting Communities";function r(e){o.useEffect(()=>(document.title=e?`${e} | FoodShare`:t,()=>{document.title=t}),[e])}export{r as u};
